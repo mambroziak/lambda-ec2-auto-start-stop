4c24822c53dce56aa0aa3502f172bc6204993044
Matt A <mambroziak@users.noreply.github.com>
2020-12-21 22:21:44 -0500

    tag support

--- lambda_function.py
+++ lambda_function.py
@@ -2,7 +2,17 @@ import boto3
 import json
 import os
 
-def toggle_instances(region, action, instances):
+# set ec2_tag=shoutcast
+# set region_list=sa-east-1,us-west-2
+# set action=start
+
+ec2_tag = os.environ.get("ec2_tag")
+region_list = os.environ.get("region_list").split(",")  # comma-delimited value
+action = os.environ['action']
+
+
+def toggle_instances(region, toggle_action, instances):
+
     session = boto3.Session(
         region_name=region
     )
@@ -10,20 +20,59 @@ def toggle_instances(region, action, instances):
     client = session.client('ec2')
     
     response = 'None'
-    if action == 'start':
+    if toggle_action == 'start':
         response = client.start_instances(InstanceIds=instances)
-    elif action == 'stop':
+    elif toggle_action == 'stop':
         response = client.stop_instances(InstanceIds=instances) 
     else:
         print('Action not supported. Must be start or stop')
         
     print(response)
     return response
-    
+
+
+def get_instance_list(region):
+
+    instance_list = []
+
+    session = boto3.Session(
+        region_name=region
+    )
+
+    ec2 = session.resource('ec2')
+
+    instances = ec2.instances.filter(Filters=[{
+        'Name': 'tag:'+ec2_tag,
+        'Values': ['*']}])
+
+    for instance in instances:
+        instance_list.append(instance.id)
+
+    print(instance_list)
+    return instance_list
+
+
+def run_now():
+
+    for region in region_list:
+        this_list = get_instance_list(region)
+        if this_list:
+            toggle_instances(region, action, this_list)
+        else:
+            print('no instances found with specified tag')
+
+
 def lambda_handler(event, context):
 
-    toggle_instances('sa-east-1', os.environ['action'], ['i-087b4f88e3633dfd2', 'i-02d0ef71d54f954b0'], )
-    toggle_instances('us-west-2', os.environ['action'], ['i-0f39d9b1d0ae87e78', 'i-046c4cf1a9acf8290', 'i-02b2bb9f6310b9ebb','i-043cbe361b0a7ac67'])
-    
-    rtnmsg = 'Completed Instances action of: ' + os.environ['action']
-    return {'status': rtnmsg}
+    run_now()
+
+    return_message = 'Completed Instances action of: ' + action
+    return {'status': return_message}
+
+
+def main():
+
+    run_now()
+
+
+if __name__ == '__main__': main()
